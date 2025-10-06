d9569e279796de5b5298c560b3a49db0d2670e9a
zazinho11 <Abidi.Aziz@esprit.tn>
2025-10-06 14:02:58 +0100

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "geszone.h"
+#include <QTableWidget>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,13 +14,50 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void supprimerEtDecalerLignes(); // supprime la ligne et décale le reste
+
+    void on_pushButton_clicked();
+    void on_pushButton2_clicked();
+    void on_pushButton3_clicked();
+    void on_on_pushButton_clicked();
+    void on_on_pushButton_2_clicked();
+    void on_on_pushButton_3_clicked();
+    void on_Ghabitant_clicked();
+    void on_Ghabitats_2_clicked();
+    void on_Gpersonnels_2_clicked();
+    void on_Gequipements_2_clicked();
+    void on_Gvehicules_2_clicked();
+    void on_Gespace_clicked();
+    void on_on_pushButton_4_clicked();
+    void on_on_pushButton_5_clicked();
+    void on_on_pushButton_6_clicked();
+    void on_on_pushButton_7_clicked();
+    void on_on_pushButton_8_clicked();
+    void on_on_pushButton_9_clicked();
+    void on_gespub_2_clicked();
+    void on_geszone1_clicked();
+
+    void on_pushButton_30_clicked();
+
+    void on_on_pushButton_13_clicked();
+
+    void on_on_pushButton_14_clicked();
+
+    void on_on_pushButton_15_clicked();
+
+    void on_on_pushButton_16_clicked();
+
+    void on_on_pushButton_17_clicked();
+
+    void on_pushButton_15_clicked();
 
 private:
     Ui::MainWindow *ui;
-
-    geszone* geszoneWindow;  // only remaining window
+    void setupTable(); // tri + croix
 };
 
 #endif // MAINWINDOW_H
